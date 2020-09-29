265ab4b5e25664d1600d6f784eb80a84e9918030
VladaIgnatyeva <vlada.ignatyeva7@gmail.com>
2020-09-29 20:59:27 +0300

    initialized project and create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Header } from './components/Header/Header';
+import { Login } from './pages/Login/Login';
+import { Info } from './pages/Info/Info';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router >
+        <Header />
+        <Route exact path='/'>
+
+        </Route>
+        <Route path='/login' component={Login} />
+        <Route path='/info' component={Info} />
+      </Router>
+    </>
   );
 }
 
